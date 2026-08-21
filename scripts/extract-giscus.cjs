de4382d90d165a7d5edd69883531648803dededf
chaggle <chaggle@users.noreply.github.com>
2026-08-21 22:05:53 +0800

    deploy: Fuwari/Astro build output (source moved to chaggle/blog)

--- scripts/extract-giscus.cjs
+++
@@ -1,3 +0,0 @@
-const html = require('fs').readFileSync('dist/posts/2026/08/14/agent-harness-learning-guide/index.html', 'utf8');
-const i = html.indexOf('giscus');
-console.log(html.slice(i - 100, i + 1300));
